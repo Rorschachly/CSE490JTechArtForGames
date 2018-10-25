15fd2059efbcee2c7355f19bee13479051cefb4f
Jose Pacio <pacioj@attu.cs.washington.edu>
2018-10-25 15:22:17 -0700

    added lighting and post processing to project

---
+++ HW1and2/Assets/Scripts/AnimatePost.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class AnimatePost : MonoBehaviour
+{
+    PostProcessVolume volume;
+    ColorGrading colorGrade;
+
+    public float duration = 2.0f;
+
+    float startValue;
+    public float endValue;
+    float t = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        volume = GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out colorGrade);
+        startValue = colorGrade.saturation.value;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime / (duration);
+        colorGrade.saturation.value = Mathf.Lerp(startValue, endValue, t);
+        Debug.Log(colorGrade.saturation.value);
+    }
+}
